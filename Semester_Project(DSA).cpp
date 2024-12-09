a8f0890a43aacb7d4f662e0bb7aa5937e35f3ad3
mzayan-bit <muhammadzayan2004@gmail.com>
2024-12-09 20:33:48 +0500

    implemented add and remove friend

--- Semester_Project(DSA).cpp
+++ Semester_Project(DSA).cpp
@@ -1,51 +1,66 @@
-#include<iostream> //For input and output objects.
-#include<fstream> //used for both "ofstream" and "if isdtream" operations.
-#include<string> //For operations on string arrays.
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-struct Friend //Created a friend struct here more friends' information will be added later like i.e age, hobbies etc.
+struct User // Using structure to represent a user
 {
     string name;
-    Friend* next;
+    string* interests[3]; // We have fixed-size array(3) for interests.
 };
 
-class Friends_made_List
+struct Friend // Using structure to represent a friend
 {
-    Friend* head; //Creating pointer object of Friend struct here to call all friends' data in a file.
-    
-    void saving_friend_info() //A function that saves friend list to a file using link list traversal concept.
+    string name;
+    int friendshipLevel;
+    string* interests[3]; // We also have fixed-size(3) array for interests.
+};
+
+struct Friend_node //This is our struct for a link list node.
+{
+    Friend data;
+    Friend_node* next;
+
+    Friend_node(Friend all_values_in_friend):data(all_values_in_friend),next(NULL){} // Initializing Friend object and next pointer here.
+};
+/// @brief Used addfriend, removefriend, and displayfriend functions here.
+class Friend_list // Implementation of Linked list here.
+{
+public:
+    Friend_node* head;
+
+    Friend_list():head(NULL){}//Initializing head in the constructor.
+
+    void addingfriend(Friend f)
     {
-        ofstream file("friends.txt"); //File name is considered to be "friends.txt".
-        if(!file) 
-        {
-            cout<<"Error opening file!"<<endl;//If the file does not exist.
-            return;
-        }
-        Friend* temp=head;
-        while(temp!=nullptr) //Traversing and saving all friend names here into our "txt" file.
-        {
-            file<<temp->name<<endl;
-            temp=temp->next;
-        }
-        file.close(); //Closing the file after saving.
+        Friend_node* newnode=new Friend_node(f); //Creating new node dynamically here.
+        newnode->next=head;
+        head=newnode; //Initializing new node as head here.
     }
-    // Helper function to load friends from a file
-    void loading_friend_info()
+
+    void removingfriend(const string& name)
     {
-        ifstream file("friends.txt");
-        if(!file) //No file or file empty, thus nothing to load.
+        //Creating two nodes here to one to be deleted and other keep track of our link list.
+        Friend_node* current=head; 
+        Friend_node* prev=NULL;
+
+        while(current)
         {
-            return; 
+            if(current->data.name==name)//Checking our basic condition here.
+            {
+                if(prev)
+                {
+                    prev->next=current->next; //Creating direct link between previous and next node of the node to be deleted, thus skipping it entirely.
+                }
+                else //The head node is to be deleted.
+                {
+                    head=current->next;
+                }
+                delete current; //freeing up head.
+                return;
+            }
+            prev=current; //For traversing through our entire linked list.
+            current=current->next;//Same as above comment.
         }
-        string name; 
-        //getline(Where are you recieving input form,saving destination).
-        while(getline(file, name)) //Using getline function here to store full name(Sperate words in name).
-        {
-            addFriend(name); // Add each friend loaded from the file
-        }
-        file.close();
-    }
-    public:
-        void addFriend(const string& friend_name){}//Add friend function to be updated later.
-};        
+    }};
